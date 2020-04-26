314bcb02788087e538fcaef23c7369d3e0625de8
Lei Wang <leiwng@gmail.com>
2020-04-26 17:29:35 +0800

    first complete

---
+++ template/Vagrantfile.j2.rb
@@ -0,0 +1,33 @@
+Vagrant.configure("2") do |config|
+
+  config.vm.provider :virtualbox do |v|
+    v.name = {{ vmName }}
+    v.memory = {{ vmMem }}
+    v.cpus = {{ vmCpu }}
+  end
+
+  config.vm.define :master do |master|
+    master.vm.box = {{ vmBox }}
+    master.vm.box_url = {{ vmUrl }}
+    master.vm.hostname = {{ vmHostname }}
+    master.vm.network "public_network", ip: {{ vmPubIP }}
+    master.vm.synced_folder {{ vmSyncDirOnHost }}, {{ vmSyncDirOnGuest }}
+    master.vm.provision "shell", inline: $setBaseEnv
+  end
+
+end
+
+$setBaseEnv = <<-SCRIPT
+
+sudo setenforce 0
+sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+sudo systemctl stop firewalld & systemctl disable firewalld
+sudo modprobe br_netfilter
+sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+sudo sysctl -w net.ipv4.ip_forward=1
+
+sudo sysctl --system
+sudo sysctl -p
+
+SCRIPT
