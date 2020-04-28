c25904d368a039c1c2ddb14e596f2869d006d8a0
Lei Wang <leiwng@gmail.com>
2020-04-28 13:27:40 +0800

    copy control host ssh pub key to taget hosts and add to autherized_keys of root user.

--- template/Vagrantfile.j2.rb
+++ template/Vagrantfile.j2.rb
@@ -10,7 +10,7 @@ Vagrant.configure("2") do |config|
     master.vm.box = "{{ vmBox }}"
     master.vm.box_url = "{{ vmUrl }}"
     master.vm.hostname = "{{ vmHostname }}"
-    master.vm.network "public_network", ip: "{{ vmPubIP }}"
+    master.vm.network "public_network", ip: "{{ vmPubIP }}", bridge: "{{ vmBridge }}"
     master.vm.synced_folder "{{ vmSyncDirOnHost }}", "{{ vmSyncDirOnGuest }}"
     master.vm.provision "shell", inline: $setBaseEnv
   end
@@ -31,4 +31,12 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo sysctl --system
 sudo sysctl -p
 
+sudo touch /root/.ssh/authorized_keys
+sudo chmod 666 /root/.ssh/authorized_keys
+sudo cat {{ vmSyncDirOnGuest }}/{{ ctlSSHKeyName }} >> /root/.ssh/authorized_keys
+sudo chmod 600 /root/.ssh/authorized_keys
+
 SCRIPT
+
+# 选择网络不能自动
+# 新建立的虚机要配置ssh免密登录
