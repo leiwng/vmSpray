1b880fd15eee75ec62902166f31569761b0debbc
Lei Wang <leiwng@gmail.com>
2020-04-29 17:33:49 +0800

    only deal with 77 vm ip chg to 192.168.0.77 to avoid DUP! issue on 192.168.0.28

--- template/Vagrantfile.j2.rb
+++ template/Vagrantfile.j2.rb
@@ -23,20 +23,38 @@ $setBaseEnv = <<-SCRIPT
 # sudo setenforce 0
 # sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
+# stop and disable filewalld
 sudo systemctl stop firewalld & systemctl disable firewalld
 sudo modprobe br_netfilter
+# set IPv4 forward
 sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 sudo sysctl -w net.ipv4.ip_forward=1
 
+# make chg available
 sudo sysctl --system
 sudo sysctl -p
 
+# make ssh without password
 sudo touch /root/.ssh/authorized_keys
 sudo chmod 666 /root/.ssh/authorized_keys
 sudo cat {{ vmSyncDirOnGuest }}/{{ ctlSSHKeyName }} >> /root/.ssh/authorized_keys
 sudo chmod 600 /root/.ssh/authorized_keys
 
+# fix issue : Ubuntu package install - "No package matching 'aufs-tools' is available"
+## get apt gpg key
+curl -s -o apt-key.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo apt-key add ./apt-key.gpg
+## replace source list
+### backup source list
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+### get source list from ustc
+curl -s -o sources.list https://mirrors.ustc.edu.cn/repogen/conf/ubuntu-https-4-xenial
+### replace system sources list
+sudo cp ./sources.list /etc/apt/.
+## make sources list available
+# sudo apt-get update
+# sudo apt-get upgrade
+
 SCRIPT
 
-# 选择网络不能自动
-# 新建立的虚机要配置ssh免密登录
+# 需要将新建虚拟机添加到本机known_hosts
